34e0a907284770e4753132845f37d1b56c95a3dc
ctorney <colin.j.torney@gmail.com>
2023-01-21 13:44:50 +0000

    pdf loading

--- app/src/main/java/com/onyx/dailydiary/MainActivity.java
+++ app/src/main/java/com/onyx/dailydiary/MainActivity.java
@@ -2,6 +2,8 @@ package com.onyx.dailydiary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.preference.PreferenceManager;
@@ -11,6 +13,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -21,10 +28,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 //import android.widget.Toolbar;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 
 import androidx.appcompat.widget.Toolbar;
 
@@ -46,10 +57,13 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
     private RecyclerView calendarRecyclerView;
     private LocalDate selectedDate;
 
+    private String DayofMonth;
+
 
 //    private AppBarConfiguration appBarConfiguration;
 //    private ActivityMainBinding binding;
-
+//    private String filename = "SampleFile.pdf";
+    private String filepath = "DailyNotes";
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -57,6 +71,10 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
         setContentView(R.layout.activity_main);
         initWidgets();
         selectedDate = LocalDate.now();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd");
+        DayofMonth = selectedDate.format(formatter);
+
         setMonthView();
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -64,11 +82,42 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences sharedPref =
-                        PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-                String  switchPref = sharedPref.getString("ics_url", "");
-                Snackbar.make(view, switchPref, Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+
+
+                try {
+
+                    String filename =  DayofMonth + "-" + monthYearFromDate(selectedDate) + ".pdf";
+
+                    File myExternalFile = new File(getExternalFilesDir(filepath), filename);
+
+                    if (!myExternalFile.exists())
+                        generatePDF(myExternalFile);
+
+                    Uri path = FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID.toString() + ".provider", myExternalFile);
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setDataAndType(path,"application/pdf");
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    startActivity(intent);
+
+
+
+                }
+                catch (Exception e)
+                {
+
+                    Toast.makeText(MainActivity.this, "Unable to open daily notes file.", Toast.LENGTH_LONG).show();
+
+                }
+
+//                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+//                String  numPages = sharedPref.getString("num_pages", "2");
+//
+//
+//                pdfhandler.openPDF(MainActivity.this, DayofMonth + "-" + monthYearFromDate(selectedDate), numPages);
+
             }
         });
 
@@ -101,8 +150,7 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
                 // opening a new intent to open settings activity.
                 Intent i = new Intent(MainActivity.this, SettingsActivity.class);
                 startActivity(i);
-                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT)
-                        .show();
+
                 break;
             default:
                 break;
@@ -147,13 +195,25 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
 
     private String monthYearFromDate(LocalDate date)
     {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM-yyyy");
+        return date.format(formatter);
+    }
+
+    private String dayMonthYearFromDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
         return date.format(formatter);
     }
 
     private String monthFromDate(LocalDate date)
     {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM");
+        return date.format(formatter);
+    }
+
+    private String yearFromDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy");
         return date.format(formatter);
     }
 
@@ -174,10 +234,103 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
     {
         if(!dayText.equals(""))
         {
-            String message = "Selected Date " + dayText + " " + monthYearFromDate(selectedDate);
+            DayofMonth = dayText;
+            String message = "Selected Date " + DayofMonth + " " + monthYearFromDate(selectedDate);
+//            String message = "Selected Date "  + dayMonthYearFromDate(selectedDate);
             Toast.makeText(this, message, Toast.LENGTH_LONG).show();
         }
     }
 
+    private void generatePDF(File file) {
+
+        PdfDocument pdfDocument = new PdfDocument();
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        String numPagesString = sharedPref.getString("num_pages", "2");
+
+        int numPages = 2;
+        try {
+            numPages = Integer.parseInt(numPagesString);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(MainActivity.this, "Unable to parse num pages value " + numPagesString + ". Using default value of 2.", Toast.LENGTH_LONG).show();
+        }
+
+
+
+        int pageHeight = 2200;
+        int pageWidth = 1650;
+
+        int linesStart = 140;
+        int linesStop = pageHeight;
+        int lineHeight = 60;
+
+
+        PdfDocument.PageInfo myPageInfo;
+        PdfDocument.Page startPage;
+
+        // setup first page
+        myPageInfo = new PdfDocument.PageInfo.Builder(pageWidth, pageHeight, 1).create();
+        startPage = pdfDocument.startPage(myPageInfo);
+
+        Paint title = new Paint();
+
+        title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+        title.setTextSize(40);
+        title.setColor(ContextCompat.getColor(this, R.color.black));
+
+        Canvas canvas;
+        canvas = startPage.getCanvas();
+        canvas.drawText(DayofMonth + " " + monthFromDate(selectedDate)+ " " + yearFromDate(selectedDate), 110, 95, title);
+        Paint paint;
+        paint = new Paint();
+
+        paint.setColor(ContextCompat.getColor(this, R.color.gray));
+
+        canvas.drawLine(100, 0,100,pageHeight, paint);
+        for (int j = linesStart; j <= linesStop; j += lineHeight) {
+            canvas.drawLine(0, j, pageWidth, j, paint);
+        }
+
+        pdfDocument.finishPage(startPage);
+
+        // add lines to other pages
+        for (int i = 1; i < numPages; i++){
+            myPageInfo = new PdfDocument.PageInfo.Builder(pageWidth, pageHeight, i+1).create();
+            startPage = pdfDocument.startPage(myPageInfo);
+
+            canvas = startPage.getCanvas();
+            paint = new Paint();
+            paint.setColor(ContextCompat.getColor(this, R.color.gray));
+            canvas.drawLine(100, 0,100,pageHeight, paint);
+
+
+            for (int j = linesStart; j <= linesStop; j += lineHeight) {
+                canvas.drawLine(0, j, pageWidth, j, paint);
+            }
+
+            pdfDocument.finishPage(startPage);
+
+        }
+
+
+
+        try {
+            // after creating a file name we will
+            // write our PDF file to that location.
+            pdfDocument.writeTo(new FileOutputStream(file));
+
+            // below line is to print toast message
+            // on completion of PDF generation.
+            Toast.makeText(MainActivity.this, "PDF file generated successfully.", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            // below line is used
+            // to handle error
+            e.printStackTrace();
+        }
+        // after storing our pdf to that
+        // location we are closing our PDF file.
+        pdfDocument.close();
+    }
 
 }
